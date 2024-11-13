17ba32a8af325fd224a6a70c2d2d0eea0508e87b
srijan saxena <ssaxena2@volvocars.com>
2024-11-13 19:23:15 +0100

    added resizing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import FlipBook from './FlipBook';
@@ -6,6 +6,83 @@ import HTMLFlipBook from 'react-pageflip';
 import FlipBookSample from './FliipBookSample';
 
 function App() {
+  
+  const maxWidth = window.innerWidth;
+  const desktopAspectRatio = 1.2;
+  const mobileAspectRatio = 1.5;
+  const deskTopWidthDivider = 0.3;
+  const mobileWidthDivider = 0.8;
+  // const bookWidth = maxWidth * 0.3;
+  // const bookHeight = bookWidth * aspectRatio;
+
+
+  
+  const [paddingLeft,setPaddingLeft] = useState(0);
+  const [isMobile,setIsMobile] = useState<boolean|null>(null);
+  const [paddingRight,setPaddingRight] = useState(0);
+  const [bookWidth,setBookWidth] = useState(maxWidth * deskTopWidthDivider);
+  const [bookHeight,setBookHeight] = useState(bookWidth * desktopAspectRatio);
+
+  const adjustPadding=(isMobile:boolean|null,event:{data:number})=>{
+    if(!isMobile)
+    {if(event.data==9){
+      setPaddingLeft(bookWidth);
+      setPaddingRight(0);
+    }
+    else if(event.data==0){
+      setPaddingLeft(0);
+      setPaddingRight(bookWidth);
+    }
+    else{
+      setPaddingLeft(0);
+      setPaddingRight(0);
+    }}
+    else{
+      setPaddingLeft(0);
+      setPaddingRight(0);
+    }
+    console.log("adjusting padding",event.data,paddingLeft, isMobile);
+    
+  }
+
+  const adjustSize=(isMobile:boolean|null)=>{
+    if(!isMobile){
+      const calcWidth = maxWidth*deskTopWidthDivider
+      const calcHeight = calcWidth*desktopAspectRatio
+
+      setBookWidth(calcWidth);
+      setBookHeight(calcHeight);
+    }
+    else if(isMobile){
+      const calcWidth = maxWidth*mobileWidthDivider
+      const calcHeight = calcWidth*mobileAspectRatio
+
+      setBookWidth(calcWidth);
+      setBookHeight(calcHeight);
+      
+    }
+  }
+  
+  const checkIfMobile = () => {
+    return window.innerWidth < 600;
+  };
+  useEffect(() => {
+    setIsMobile(checkIfMobile());
+    adjustSize(isMobile);
+    adjustPadding(isMobile, { data: 0 });
+
+    const handleResize = () => {
+      
+      setIsMobile(checkIfMobile());
+      adjustSize(isMobile);
+      adjustPadding(isMobile, { data: 0 });
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, [isMobile]);
+
+  
   const pages = [
     <div style={{ background: "red", height: "300px", width: "100px" }}><h1>Cover Page</h1></div>, <div style={{ background: "blue", height: "300px", width: "100px" }}><p>Table of Contents1 </p></div>,
     <div style={{ background: "blue", height: "300px", width: "100px" }}><h1>page2</h1></div>, <div style={{ background: "yellow", height: "300px", width: "100px" }}><p>Table of Contents2</p></div>,
@@ -17,12 +94,16 @@ function App() {
   return (
 
     <div className="flipbook-container">
-    <FlipBookSample />
+      <div style={{ paddingLeft: paddingLeft, paddingRight: paddingRight, transition: 'padding 0.5s ease' }}>
+
+        {isMobile!=null && <FlipBookSample adjustPadding={adjustPadding} bookHeight={bookHeight} bookWidth={bookWidth} isMobile={isMobile} />}
+       
+      </div>
+
+     
   </div>
 
-    // <HTMLFlipBook width={300} height={500} style={{ width: "300px", }} className={''} startPage={0} size={'fixed'} minWidth={300} maxWidth={0} minHeight={0} maxHeight={0} drawShadow={true} flippingTime={1000} usePortrait={false} startZIndex={0} autoSize={false} maxShadowOpacity={1} showCover={false} mobileScrollSupport={true} clickEventForward={false} useMouseEvents={true} swipeDistance={1} showPageCorners={false} disableFlipByClick={false} >
-    //   {pages}
-    // </HTMLFlipBook>
+   
   );
 }
 
--- src/FliipBookSample.tsx
+++ src/FliipBookSample.tsx
@@ -1,13 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PageFlip from 'react-pageflip';
 import { Box, Typography } from '@mui/material';
 
-const FlipBookSample: React.FC = () => {
+type BookProps = {
+  adjustPadding: (isMobile:boolean,event: { data: number }) => void;
+  bookWidth:number;
+  bookHeight:number;
+  isMobile: boolean
+};
+function FlipBookSample(props:BookProps)  {
   // Define a set of colors for each page
-  const colors = ['#FFCDD2', '#C8E6C9', '#BBDEFB', '#FFF9C4', '#D1C4E9'];
+  const generateRandomColor = () => {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  };
+  const {bookHeight,bookWidth} = props
+
+  const colors = Array.from({ length: 10 }, generateRandomColor);
+  // const colors = ['#FFCDD2', '#C8E6C9', '#BBDEFB', '#FFF9C4', '#D1C4E9'];
+  console.log("joined with ",props.isMobile)
 
   return (
-    <PageFlip width={400} height={500} className="flipbook"  style={{ width: "300px", }} startPage={0} size={'fixed'} minWidth={300} maxWidth={0} minHeight={0} maxHeight={0} drawShadow={true} flippingTime={1000} usePortrait={true} startZIndex={0} autoSize={false} maxShadowOpacity={1} showCover={false} mobileScrollSupport={true} clickEventForward={false} useMouseEvents={true} swipeDistance={0.5} showPageCorners={true} disableFlipByClick={true} >
+    <PageFlip width={bookWidth} height={bookHeight}
+      className="flipbook-page"
+      style={{}}
+      startPage={0}
+      size={'stretch'}
+      minWidth={bookWidth}
+      maxWidth={bookWidth}
+      minHeight={bookHeight}
+      maxHeight={bookHeight}
+      drawShadow={true}
+      flippingTime={1000}
+      usePortrait={props.isMobile} // affects if single page or not
+      startZIndex={0}
+      autoSize={false}
+      maxShadowOpacity={1}
+      showCover={true}
+      mobileScrollSupport={props.isMobile}
+      clickEventForward={false}
+      onFlip={(flipEvent)=>props.adjustPadding(props.isMobile,flipEvent)}
+      useMouseEvents={true}
+      swipeDistance={0.5}
+      showPageCorners={true}
+      disableFlipByClick={true} >
       {colors.map((color, index) => (
         <Box
           key={index}
